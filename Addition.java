6b4b9257062049d0d6b0c5781d564ba7227282a7
Pavankumarparamkusam <paramkusampavankumar1@gmail.com>
2024-07-11 16:30:50 +0530

    My first repositiory stored successfullu

---
+++ Addition.java
@@ -0,0 +1,8 @@
+class Addition{
+	public static void main(String args[]){
+		int a=20;
+		int b=30;
+		int add=a-b;
+		System.out.println("The Addition of two numbers is:"+add);
+	}
+}
---
+++ Subtraction.java
@@ -0,0 +1,8 @@
+class Subtraction{
+	public static void main(String args[]){
+		int a=20;
+		int b=30;
+		int sub=a-b;
+		System.out.println("The Subtraction of two numbers is:"+sub);
+	}
+}
